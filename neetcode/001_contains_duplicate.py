0d275a3d22e8f2fb02ef5e46619d0c1acb18550d
samrathsingh302 <samrathsingh302@gmail.com>
2026-09-03 01:44:30 +0100

    neetcode 001: re-indent contains_duplicate (uncommitted since 30/08, swept by hygiene 03/09)

--- neetcode/001_contains_duplicate.py
+++ neetcode/001_contains_duplicate.py
@@ -1,11 +1,10 @@
 def contains_duplicate(nums):
-    seen = []
-    for num in nums: 
-       if num in seen:
-         return True
-       else:
-         seen.append(num)
-    return False
+  seen = []
+  for num in nums:
+    if num in seen:
+      return True
+    seen.append(num)
+  return False
 
 print(contains_duplicate([1, 2, 3, 1]))
 print(contains_duplicate([1, 2, 3]))
